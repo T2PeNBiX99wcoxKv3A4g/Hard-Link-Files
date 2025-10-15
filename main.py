b8c1f128bae6d4c7839e44df55d33c45a8a9b5ba
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2025-10-15 15:21:44 +0800

    init: add scripts

---
+++ main.py
@@ -0,0 +1,52 @@
+import os
+
+import typer
+from icecream import ic
+
+app = typer.Typer()
+
+
+class NotAFolderException(Exception):
+    pass
+
+
+reference_files: list[str] = []
+
+
+def debug_output_control(debug: bool):
+    if debug:
+        ic.enable()
+    else:
+        ic.disable()
+
+
+@app.command()
+def create_hard_link_files(reference_folder_path: str, source_folder_path: str, link_folder_path: str,
+                           debug: bool = False):
+    debug_output_control(debug)
+    if not os.path.isdir(reference_folder_path):
+        raise NotAFolderException(f"Reference path need to be folder.")
+    if not os.path.isdir(source_folder_path):
+        raise NotAFolderException(f"Source path need to be folder.")
+    if not os.path.isdir(link_folder_path):
+        raise NotAFolderException(f"Link path need to be folder.")
+    for _, _, files in os.walk(reference_folder_path):
+        for file in files:
+            ic(file)
+            reference_files.append(file)
+
+    for file in reference_files:
+        source_path = ic(os.path.join(source_folder_path, file))
+        link_path = ic(os.path.join(link_folder_path, file))
+        if not os.path.isfile(source_path):
+            typer.echo(f"File {source_path} not found. skipping...")
+            continue
+        if os.path.isfile(link_path):
+            typer.echo(f"File {link_path} already exists. skipping...")
+            continue
+        os.link(source_path, link_path)
+        typer.echo(f"{source_path} -> {link_path}")
+
+
+if __name__ == '__main__':
+    app()
